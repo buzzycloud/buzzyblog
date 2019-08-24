8aa1b6648c1589e9ed434a92d9183cf79120b676
medmin <erichui329@gmail.com>
2019-08-23 21:27:10 -0700

    meta data of posts displayed

--- src/components/posts/PostContainer.js
+++ src/components/posts/PostContainer.js
@@ -1,9 +1,32 @@
 import React, { useContext } from "react";
+import parse from "html-react-parser";
 import { PostContext } from "@/contexts/PostContext";
 
 const PostContainer = () => {
     const { posts } = useContext(PostContext);
-    return <h6>Post List</h6>;
+    console.log(posts);
+    return (
+        <div className="tile is-parent">
+            <div className="container">
+                {posts.map((post) => {
+                    return (
+                        <div className="tile is-child is-8" key={post.id}>
+                            <div style={{ marginBottom: "0.5rem" }} className="title">
+                                {post.title.rendered}
+                            </div>
+                            <div className="tags is-marginless">
+                                <span className="tag is-rounded ">{post.date.replace("T", " ")}</span>
+                            </div>
+                            <div>{parse(post.excerpt.rendered)}</div>
+                            <div className="tags" style={{ marginBottom: "1rem" }}>
+                                {post.tags.length ? <span className="tag is-success">{post.tags}</span> : ""}
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default PostContainer;
