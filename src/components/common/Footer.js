bb82af50b992cc139fcbf862ea84b515c86e2d39
medmin <erichui329@gmail.com>
2019-08-25 12:40:04 -0700

    update footer wording

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
             <div className="container">
                 <div className="content has-text-centered">
                     <p>
-                        <strong>Buzzy Notes</strong> by <a href="https://guiyumins.com">Yumin Gui</a>. The source code
-                        is licensed under
+                        <strong>Buzzy Blog</strong> by <a href="https://guiyumin.com">Yumin Gui</a>. The{" "}
+                        <a href="https://github.com/buzzycloud">source code</a> is licensed under
                         <a href="https://www.apache.org/licenses/LICENSE-2.0"> Apache 2.0</a>. The website content is
                         licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
                     </p>
