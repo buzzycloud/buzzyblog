005d54bf830df48f8d6aaf2ae2932892f1fda710
medmin <erichui329@gmail.com>
2019-09-08 08:44:40 -0700

    toggle padding when desktop and mobile

--- src/components/routes/Homepage.js
+++ src/components/routes/Homepage.js
@@ -2,10 +2,13 @@ import React from "react";
 import SideBar from "@/components/common/SideBar";
 import PostContainer from "@/components/posts/PostContainer";
 
+import "@/assets/css/global.css";
+import "@/assets/css/routes-Homepage.css";
+
 const Homepage = () => {
     return (
-        <div className="tile is-parent is-flex-widescreen">
-            <div className="tile is-3" style={{ padding: "0.75rem 0" }}>
+        <div className="tile is-parent is-flex-widescreen homepage-is-left-paddingless-desktop">
+            <div className="tile is-3 is-x-paddingless">
                 <SideBar />
             </div>
             <div className="tile is-9">
