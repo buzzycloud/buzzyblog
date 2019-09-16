f9bb59d9bae6d29c60f512fc944c45b81ff6eac3
medmin <erichui329@gmail.com>
2019-09-15 21:43:34 -0700

    more meaning names in server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const next = require("next");
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
-app.prepare().then(() => {
+const next = require("next")({ dev });
+const handle = next.getRequestHandler();
+
+next.prepare().then(() => {
     const server = express();
     server.get("*", (req, res) => {
         return handle(req, res);
