cc6845940fdcc60c38b836f4d50b416ae993e8f7
medmin <erichui329@gmail.com>
2019-09-15 21:05:23 -0700

    setup express server

---
+++ pages/resume.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Resume from "../src/components/myself/Resume";
+import "bulma/css/bulma.min.css";
+
+const Index = () => {
+    return (
+        <React.Fragment>
+            <Resume />
+            <Resume />
+        </React.Fragment>
+    );
+};
+
+export default Index;
