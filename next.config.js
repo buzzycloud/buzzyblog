5c862e5ad79c91d4af3206eb16860d12283fc359
medmin <erichui329@gmail.com>
2019-10-08 19:18:55 -0700

    nextjs bugfix

--- next.config.js
+++ next.config.js
@@ -2,19 +2,6 @@ const compose = require("lodash/fp/compose");
 const withSass = require("@zeit/next-sass");
 const withCss = require("@zeit/next-css");
 
-function HACK_removeMinimizeOptionFromCssLoaders(config) {
-    console.warn("HACK: Removing `minimize` option from `css-loader` entries in Webpack config");
-    config.module.rules.forEach((rule) => {
-        if (Array.isArray(rule.use)) {
-            rule.use.forEach((u) => {
-                if (u.loader === "css-loader" && u.options) {
-                    delete u.options.minimize;
-                }
-            });
-        }
-    });
-}
-
 const enhance = compose(
     withCss,
     withSass
@@ -40,7 +27,8 @@ module.exports = enhance({
     poweredByHeader: false,
     webpack(config, options) {
         config.module.rules.push(fileLoaderRule);
-        HACK_removeMinimizeOptionFromCssLoaders(config); //http://bit.ly/nextjs-css-loader-bug
         return config;
     },
 });
+
+//http://bit.ly/nextjs-css-loader-bug this bug is fixed in v9.1.2-canary.1
